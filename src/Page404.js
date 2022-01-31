d900b44afeed43de731977534d91342e44389dbc
Leandro <leandroquiroga9514@gmail.com>
2022-01-30 21:21:34 -0300

    feat: Page not found if case a route does't exit

---
+++ src/Page404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Page404 = () => {
+  return (
+    <div className='container'>
+      <h3 className='fw-bold text-center text-uppercase'>Pagina no encontrada</h3>
+    </div>
+  );
+};
