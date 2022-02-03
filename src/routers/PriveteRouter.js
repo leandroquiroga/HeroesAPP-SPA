cbc7a9efb14e7aac4d9394ac70b95b5463bd4db6
Leandro <leandroquiroga9514@gmail.com>
2022-02-03 15:57:53 -0300

    New: Has benn a component of Private Router

---
+++ src/routers/PriveteRouter.js
@@ -0,0 +1,27 @@
+
+
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../auth/autoContext';
+import { Navigate, useLocation } from 'react-router-dom';
+
+export const PriveteRouter = ({children}) => {
+  
+  const { user } = useContext(AuthContext);
+  const { pathname, search } = useLocation();
+  
+  // Esta atento al cambio del nuesto pathname del useLocation,
+  // cuando detecte un cambio checkea si exite el search, en caso de que 
+  // sea correcto se alamacena en el localStorage, sino se almancena el pathname
+  useEffect(() => {
+    localStorage.setItem('lastPath', pathname + (search && search) )
+  },[pathname])
+  
+  return (user.logged) ?
+          children : <Navigate to='/login'/> 
+};
+
+
+// En caso que el usuario este autenticado
+// le permite acceso a la aplicacion 
+// en caso contrario lo redirige 
+// al que dse autentifique
