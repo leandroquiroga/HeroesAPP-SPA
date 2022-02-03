1a6b87bf35d4f1df61621f425e637d311e4ea1c7
Leandro <leandroquiroga9514@gmail.com>
2022-02-03 16:06:22 -0300

    Feat: Search the localStorage for the last received pathname of useLocation

--- src/components/Login/LoginScreen.js
+++ src/components/Login/LoginScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
   
 import { useNavigate } from 'react-router-dom';
+import { types } from './../../types/type';
+import { AuthContext } from './../../auth/autoContext';
 
 export const LoginScreen = () => {
   const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
   
   //inida a que pagina se debe regresar
   const handleLogin = () => {
-    navigate('/', {
+
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Leandro'
+      },
+    };
+
+    dispatch(action);
+
+    const lastPath = localStorage.getItem('lastPath') || '/'
+
+    navigate(lastPath, {
       replace: true
     })
   }
