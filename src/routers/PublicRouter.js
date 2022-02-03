7ec5894088548fe77bf14d63d962f480a031a679
Leandro <leandroquiroga9514@gmail.com>
2022-02-03 15:58:17 -0300

    New: Has benn a component of Public Router

---
+++ src/routers/PublicRouter.js
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../auth/autoContext";
+
+
+export const PublicRouter = ({ children }) => {
+  
+  const { user } = useContext(AuthContext);
+  
+  // Si el usuario no esta autotenticado
+  // muestra el el componente de login
+  // en caso contrario muestra la pagina de inicio
+  return (!user.logged) ?
+        children
+        :<Navigate to='/' />
+};
