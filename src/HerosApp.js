86284f1745e7d5fe0b8a6f2d060549201564cec9
Leandro <leandroquiroga9514@gmail.com>
2022-02-03 16:08:23 -0300

    Init App

--- src/HerosApp.js
+++ src/HerosApp.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { AuthContext } from './auth/autoContext';
 import { AppRouter } from './routers/AppRouter';
 import 'animate.css';
+import { authReducer } from './auth/authReducer';
+
+
+// Retorna el usuario si lo encuentra en localStorage si no retorna el logged en false
+const init = () => JSON.parse(localStorage.getItem('user')) || {logged: false}
+
+
 
 export const HerosApp = () => {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  // Esta pendiente al user para ir almancendolo en el localStorage,
+  // siempre y cuando exista.
+  
+  useEffect(() => {
+    if (!user) return;
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
   return (
+    <AuthContext.Provider value={{
+      user,
+      dispatch
+    }}>
       <AppRouter />
+    </AuthContext.Provider>
   );
 };
