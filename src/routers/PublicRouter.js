51a2ad6af94dc7b58c14ec608bb4ff6accafb725
Leandro <leandroquiroga9514@gmail.com>
2022-02-08 19:26:56 -0300

    feat: Public Router

--- src/routers/PublicRouter.js
+++ src/routers/PublicRouter.js
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../auth/autoContext";
+import { AuthContext } from "../auth/authContext";
 
 
 export const PublicRouter = ({ children }) => {
