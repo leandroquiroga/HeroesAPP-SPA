3370b94fa62f799e0d4e5747454b3207fea3b5b0
Leandro <leandroquiroga9514@gmail.com>
2022-02-08 19:34:57 -0300

    Init App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HerosApp } from './HerosApp';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
