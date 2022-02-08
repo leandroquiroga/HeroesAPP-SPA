9ab3a8475cac1997d899b03725d270f9a8464806
Leandro <leandroquiroga9514@gmail.com>
2022-02-08 19:34:30 -0300

    feat: Has been added acomponent AuthProvaider

--- src/HerosApp.js
+++ src/HerosApp.js
@@ -1,32 +1,13 @@
-import React, { useEffect, useReducer } from 'react';
-import { AuthContext } from './auth/autoContext';
+import React from 'react';
+import { AuthProvaider } from './auth/authContext';
 import { AppRouter } from './routers/AppRouter';
+
 import 'animate.css';
-import { authReducer } from './auth/authReducer';
-
-
-// Retorna el usuario si lo encuentra en localStorage si no retorna el logged en false
-const init = () => JSON.parse(localStorage.getItem('user')) || {logged: false}
-
-
 
 export const HerosApp = () => {
-
-  const [user, dispatch] = useReducer(authReducer, {}, init);
-
-  // Esta pendiente al user para ir almancendolo en el localStorage,
-  // siempre y cuando exista.
-  
-  useEffect(() => {
-    if (!user) return;
-    localStorage.setItem('user', JSON.stringify(user))
-  }, [user])
   return (
-    <AuthContext.Provider value={{
-      user,
-      dispatch
-    }}>
+    <AuthProvaider>
       <AppRouter />
-    </AuthContext.Provider>
+    </AuthProvaider>
   );
 };
