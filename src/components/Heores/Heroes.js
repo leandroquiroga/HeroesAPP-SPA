7765be8d622c9022eff636f245ece6caed86c55c
Leandro <leandroquiroga9514@gmail.com>
2022-02-09 17:39:50 -0300

    feat: Has been add an icon in the button and toastify

--- src/components/Heores/Heroes.js
+++ src/components/Heores/Heroes.js
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
 import { useParams, useNavigate, Navigate } from 'react-router-dom';
 import { getHeroById } from './../../helpers/index';
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 export const Heroes = () => {
-  
+  // const [msgAdd , setMsgAdd] = useState(false)  
   const { heroID } = useParams();
   const navigate = useNavigate();
   const hero = useMemo(() => getHeroById(heroID), [heroID] ) 
@@ -15,46 +17,73 @@ export const Heroes = () => {
   };
 
   // Regresa al url anterior de la pila del stack
-  const handleBack = () => navigate(-1)
+  const handleBack = () => navigate(-1);
+
+
+  const notify = () => {
+    toast.success('Nuevo heroe agregado', {
+      theme: 'colored',
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+  }
+  
 
   const imagePath = `/assets/img/${id}.jpg`
+  const startImagePath = `/assets/svg/start.svg`
+  const backImagePath = `/assets/svg/back.svg`
   return (
     <div className='container animate__animated animate__fadeIn '>
-      <div className='row mt-3'>
-        <div className='col-4 '>
+      <div className='d-flex flex-column mt-3 flex-md-row '>
+        <div className='col-12 col-md-4 '>
           <img
             src={imagePath}
             alt={superhero}
-            className='img-thumbnail'
+            className='img-fluid w-100'
           >
           </img>
         </div>
-        <div className='col-8 p-5'>
-          <h3>{superhero}</h3>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <b>Alter ego: </b>
-                {alter_ego}
-            </li>
-            <li className='list-group-item'>
-              <b>Publisher: </b>
-                {publisher}
-            </li>
-            <li className='list-group-item'>
-              <b>First Appearance: </b>
-                {first_appearance}
-            </li>
-          </ul>
+        <div className='col-12 col-md-8 p-5'>
+            <h3>{superhero}</h3>
+            <ul className='list-group list-group-flush'>
+              <li className='list-group-item'>
+                <b>Alter ego: </b>
+                  {alter_ego}
+              </li>
+              <li className='list-group-item'>
+                <b>Publisher: </b>
+                  {publisher}
+              </li>
+              <li className='list-group-item'>
+                <b>First Appearance: </b>
+                  {first_appearance}
+              </li>
+            </ul>
 
-          <h5 className='mt-3 mx-3'> Characters </h5>
-          <p className='mx-3'> {characters} </p>
+            <h5 className='mt-3 mx-3'> Characters </h5>
+            <p className='mx-3'> {characters} </p>
 
-          <button
-            className='btn btn-danger mx-3'
-            onClick={handleBack}
-          >
-            Regresar
-          </button>
+          <div className='d-flex flex-row '>
+              <button
+                className='btn btn-danger m-2'
+                onClick={handleBack}
+              >
+                Regresar <img src={backImagePath} alt='favorite' />
+              </button>
+              <div className='d-flex flex-column flex-md-row align-items-center'>
+                <button
+                  className='btn btn-danger m-2'
+                  onClick={notify}>
+                  Añadir a <img src={startImagePath} alt='favorite' />
+              </button>
+              <ToastContainer />
+            </div>  
+          </div>
 
         </div>
       </div>
