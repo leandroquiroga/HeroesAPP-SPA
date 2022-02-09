b98346c7790354d5fde2d3602bc173676e4cb3d2
Leandro <leandroquiroga9514@gmail.com>
2022-02-09 17:48:26 -0300

    fix: Has been fixed an problem with the principal router

--- src/routers/DashboardRoute.js
+++ src/routers/DashboardRoute.js
@@ -2,18 +2,19 @@ import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { MarvelScreen } from '../components/Category/MarvelScreen';
-import { HerosApp } from '../HerosApp';
+// import { HerosApp } from '../HerosApp';
 import { DCScreen } from './../components/Category/DCScreen';
 import { SearchScreen } from './../components/Search/Search';
 import { NavbarScreen } from './../components/UI/Navbar';
 import { Heroes } from './../components/Heores/Heroes';
+import { Main } from "../components/UI/Main";
 
 export const DashboardRoute = () => {
   return (
     <Fragment>
       <NavbarScreen />
       <Routes>
-        <Route path="./" element={<HerosApp />} />
+        <Route path="/" element={<Main />} />
         <Route path="marvel" element={<MarvelScreen />} />
         <Route path="dc" element={<DCScreen />} />
         <Route path="hero/:heroID" element={<Heroes />} />
