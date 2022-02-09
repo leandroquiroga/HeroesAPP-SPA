95ac27ba9a83b8e333ddb7b5f7e3f7cb5d280199
Leandro <leandroquiroga9514@gmail.com>
2022-02-09 17:43:33 -0300

    style: Has been modificated the styles of the components

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink,  useNavigate } from 'react-router-dom';
 import { AuthContext } from './../../auth/authContext';
 import { types } from './../../types/type';
@@ -7,6 +7,7 @@ import { auth } from './../../firebase';
 
 export const NavbarScreen = () => {
 
+  const [toggle, setToggle] = useState(true)
   const navigate = useNavigate();
   const { user, dispatch, logOut } = useContext(AuthContext)
   // retorna a inicio de sesion
@@ -28,54 +29,139 @@ export const NavbarScreen = () => {
     
   };
 
-  return (
-    <nav className='navbar navbar-expand-sm navbar-dark bg-dark px-2'>
+  const imagePath = `/assets/svg/avatar.svg`
+  const logoutImagePath = `/assets/svg/logout.svg`
 
+  return (
+  <nav className='navbar navbar-expand-lg navbar-dark bg-dark px-2'>
+    <section className='container-fluid'>
       <Link
         className='navbar-brand'
-        to="/"
+        to='/'  
       >
-        Inicio
-      </Link>
-      <div className='navbar-collapse'>
-        <div className='navbar-nav'>
-          <NavLink
-            className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
-            to="/marvel"
-          >
-            Marvel
-          </NavLink>
-
-          <NavLink
-            className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
-            to='/dc'
-          >
-            DC
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
-            to='/search'
-          >
-            Search
-          </NavLink>
-      </div>
-    </div>
-    <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
-      <ul className='navbar-nav ml-auto'>
-
-        <span className='nav-item nav-link text-white'>
-            Hola, { user.name }
-            <small style={{fontSize: 20 + 'px'}}> &#x1F44B; </small>
-        </span>
-
+        <img 
+          className='img-fluid navbar-icon'
+          alt='logo navbar'
+          src={imagePath}
+        />  
+        </Link>
         <button
-          className='nav-item btn btn-danger'
-          onClick={handleLoguot}
+          className='navbar-toggler' 
+          type='button'
+          onClick={() => setToggle(!toggle)}
         >
-          Cerrar sesión
+          <span className='navbar-toggler-icon'></span>
         </button>
-      </ul>
-    </div>
+        <article
+          className={toggle ? 'collapse navbar-collapse' : 'collapse navbar-collapse  active'}
+          id="navbarSupportedContent"
+        >
+          <div className='navbar-nav text-center '> 
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to="/marvel"
+            >
+              Marvel
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to='/dc'
+            >
+              DC
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to='/search'
+            >
+              Search
+            </NavLink>
+          </div>
+          <div className='navbar-nav ms-auto' >
+            <ul className='navbar-nav'>
+
+              <span className='nav-item nav-link text-white text-center'>
+                  Hola, { user.name }
+                  <small style={{fontSize: 20 + 'px'}}> &#x1F44B; </small>
+              </span>
+
+              <button
+                className='nav-item btn btn-danger'
+                onClick={handleLoguot}
+              >
+                Cerrar sesión <img src={logoutImagePath} alt='logout' />
+              </button>
+            </ul>
+
+          </div>
+        </article>
+    </section>
   </nav>
   );
 }
+
+/*
+      <div className='container-fluid'>
+        <Link
+          className='navbar-brand'
+          to="/"
+        >
+          <img 
+            className='img-fluid navbar-icon'
+            alt='logo navbar'
+            src={imagePath}
+          />
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div
+          className='collapse navbar-collapse '
+          id="navbarSupportedContent"
+        >
+          <div className='navbar-nav'> 
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to="/marvel"
+            >
+              Marvel
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to='/dc'
+            >
+              DC
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => 'nav-item nav-link text-white' + (isActive ? 'active' : '' )}
+              to='/search'
+            >
+              Search
+            </NavLink>
+            <ul className='navbar-nav ml-auto'>
+
+              <span className='nav-item nav-link text-white'>
+                  Hola, { user.name }
+                  <small style={{fontSize: 20 + 'px'}}> &#x1F44B; </small>
+              </span>
+
+              <button
+                className='nav-item btn btn-danger'
+                onClick={handleLoguot}
+              >
+                Cerrar sesión
+              </button>
+            </ul>
+          </div>
+        </div>
+      </div>
+*/
