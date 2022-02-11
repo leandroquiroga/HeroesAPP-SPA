c796b3f35f77cb67f76477276a05ac0ce25c09d2
Leandro <leandroquiroga9514@gmail.com>
2022-02-11 13:36:32 -0300

    create: A new component showing the home page

--- src/components/UI/Main.js
+++ src/components/UI/Main.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/authContext';
+import { HeroFavourites } from './../Heores/HeroFavourites';
 
 export const Main = () => {
+
+  const { hero, dispatchHero } = useContext(AuthContext);
+  
   const pathIMG = `/assets/img/not-super-hero.png`
+  const pathCloseSVG = `/assets/svg/close.svg`
   return (
-       
-    <div className=' container d-flex justyfy-content-center align-items-center'>
+    <main className='container p-4'>
+      {(
+        (hero.length === 0) ?
+          <div className='d-flex justify-content-between align-items-center w-100 p-5 flex-column flex-lg-row '>
+            <img
+              className='img-fluid'
+              alt='have not element'
+              src={pathIMG}
+            />
 
-    {/*
-        Si hay elemento en arreglo de favoritos,
-        muestra una lista de todos los elementos 
-    
-     */}
+            <p className='text-center fs-3 mt-5 mt-lg-0'>
+              No hay superheroes en la lista de favoritos...
+            </p>
+          </div>
+        : 
+          <div>
+            <h1 className='text-center'> Superheroes favoritos </h1>
+            <hr />
+            <div
+              className="container-card"
+             
+              >
+              {(
+                  hero.map(data => (
+                    <HeroFavourites 
+                      key={data.id}
+                      data={data}
+                      pathCloseSVG={pathCloseSVG}
+                      dispatchHero={dispatchHero}
+                    />
+                  ))
+                )}
+            </div>  
+          </div>
+      )}
 
-    <div className='d-flex justify-content-between align-items-center w-100 p-5 flex-column flex-lg-row '>
-      <img 
-        className='img-fluid'
-        alt='have not element'
-        src={pathIMG}
-      />
+    </main>
+      
 
-      <p className='text-center fs-3 mt-5 mt-lg-0'>
-        No hay superheroes en la lista de favoritos...
-      </p>
-    </div>
-  </div>
   );
 };
