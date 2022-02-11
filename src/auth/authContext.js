79261ba5badd571c25adea297ab2d4d9f24b6549
Leandro <leandroquiroga9514@gmail.com>
2022-02-11 13:25:26 -0300

    fix: Has been fixed a problen with localstorage

--- src/auth/authContext.js
+++ src/auth/authContext.js
@@ -2,16 +2,20 @@ import { createContext, useEffect, useReducer, useState } from "react";
 import { signInWithEmailAndPassword, signOut ,createUserWithEmailAndPassword } from 'firebase/auth';
 import { authReducer } from "./authReducer";
 import { auth } from './../firebase';
+import { heroReducer } from "../helpers";
 
 // Retorna el usuario si lo encuentra en localStorage si no retorna el logged en false
 const init = () => JSON.parse(localStorage.getItem('user')) || {logged: false}
+
+// Retorna los hero si lo encuntra en localStorage si no retorna un array 
+const initHero = () => JSON.parse(localStorage.getItem('hero')) || [];
 
 export const AuthContext = createContext();
 
 export const AuthProvaider = (props) => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
   const [currentUser, setCurrentUser] = useState({});
-
+  const [hero, dispatchHero] = useReducer(heroReducer, [], initHero);
 
   // Cuando inicia la aplicacion va a estar observando si hay un cambio
   // de usuario.
@@ -19,12 +23,23 @@ export const AuthProvaider = (props) => {
     auth.onAuthStateChanged((user) => setCurrentUser(user))
   }, []);
 
-  // Esta pendiente al user para ir almancendolo en el localStorage,
-  // siempre y cuando exista.
+  // Esta pendiente al user para ir almancendolo en el localStorage
   useEffect(() => {
-    if (!user) return;
     localStorage.setItem('user', JSON.stringify(user))
-  }, [user])
+    return () => {
+      if (!user) return;
+    }
+  }, [user]);
+
+  // Esta pendiente al hero, cuando detecta un cambio lo almacena en el localStorage
+  useEffect(() => {
+    localStorage.setItem('hero', JSON.stringify(hero));
+    return () => {
+      if (!hero) return;
+    }
+  }, [hero])
+  
+
 
   const singUp = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 
@@ -39,6 +54,8 @@ export const AuthProvaider = (props) => {
     logIn,
     logOut,
     currentUser,
+    dispatchHero,
+    hero,
   }
   return (
     <AuthContext.Provider value={value}>
